1a92fc5beb2a33f31f467c67f30ae5d9507a6f22
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2023-01-25 20:56:29 -0500

    feat: add metatags in head

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -10,6 +10,8 @@ export const DEFAULT_PROPS: Partial<PropLayout> = {
 	headTitle: 'Pokemon App'
 };
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
+
 export const Layout: React.FC<PropLayout> = ({ children, headTitle }: PropLayout): React.ReactElement => {
 	return (
 		<>
@@ -17,7 +19,10 @@ export const Layout: React.FC<PropLayout> = ({ children, headTitle }: PropLayout
 				<title>{headTitle}</title>
 				<meta name="author" content="Bryan Aguilar" />
 				<meta name="description" content="Information about XXX" />
-				<meta name="keywords" content="XXX, pokemon" />
+				<meta name="keywords" content={`${headTitle}, pokemon`} />
+				<meta property="og:title" content="Next Poke App" />
+				<meta property="og:description" content="App created using pokeapi" />
+				<meta property="og:image" content={`${origin}/banner.png`} />
 				<link rel="shortcut icon" href="/favicon.svg" />
 			</Head>
 			<Header />
